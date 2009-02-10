48c92e8727d0fd6c9d187d6d48518bc503888a3f
jugyo <kkohno@gmail.com>
2009-02-10 23:34:05 +0900

    カレントディレクトリの設定ファイルを見るように変更

--- lib/filetter.rb
+++ lib/filetter.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 $: << File.dirname(__FILE__) + '/plugins'
 
 require 'pathname'
@@ -16,34 +16,10 @@ Thread.abort_on_exception = true
 
 module Filetter
   class << self
-    def run(options = {})
-      @options = {
-                  :conf_dir => '~/.filetter/',
-                  :conf_file_name => 'conf',
-                  :pattern => './**/*',
-                  :interval => 1,
-                  :debug => false
-                  }.merge(options)
-
-      @conf_dir = File.expand_path(@options[:conf_dir])
-      @conf_file = @conf_dir + @options[:conf_file_name]
-
-      check_conf_dir_and_file()
-      load @conf_file
-
-      Observer.run( :pattern => @options[:pattern],
-                    :interval => @options[:interval],
-                    :debug => @options[:debug] )
-    end
-
-    def check_conf_dir_and_file
-      Dir.mkdir(@conf_dir) unless File.exist?(@conf_dir)
-      $: << @conf_dir
-      unless File.exist?(@conf_file)
-        File.open(@conf_file, 'w') do |io|
-          io.puts "require 'sample'"
-        end
-      end
+    def run(conf_file = '.filetter', options = {})
+      load conf_file
+      default_option = {:pattern => './**/*', :interval => 1, :debug => false}
+      Observer.run(default_option.merge(options))
     end
   end
 end
