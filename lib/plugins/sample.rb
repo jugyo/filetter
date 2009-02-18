5835e1c3a649a5c5ba5cfac70b7dd025c83576b0
jugyo <jugyo.org@gmail.com>
2009-02-19 00:00:15 +0900

    chage to user Filetter.add_hook

--- lib/plugins/sample.rb
+++ lib/plugins/sample.rb
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 module Filetter
-  Observer.add_hook :modified do |files|
+  add_hook :modified do |files|
     print 'modified: '
     p files
   end
 
-  Observer.add_hook :created do |files|
+  add_hook :created do |files|
     print 'created: '
     p files
   end
 
-  Observer.add_hook :deleted do |files|
+  add_hook :deleted do |files|
     print 'deleted: '
     p files
   end
