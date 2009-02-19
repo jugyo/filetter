34ddd75824582525c5a8ddbd174d5f2156eeb340
jugyo <jugyo.org@gmail.com>
2009-02-19 23:56:51 +0900

    ソース整理

--- lib/modes/sample.rb
+++ lib/modes/sample.rb
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 module Filetter
   add_hook :modified do |files, event|
-    print "#{event}: "
-    p files
+    puts "#{event}: #{files.inspect}"
   end
 
   add_hook :created do |files, event|
-    print "#{event}: "
-    p files
+    puts "#{event}: #{files.inspect}"
   end
 
   add_hook :deleted do |files, event|
-    print "#{event}: "
-    p files
+    puts "#{event}: #{files.inspect}"
   end
 end
