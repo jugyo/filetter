999c57ae1e2c6d89d7195d56ca45ded367c200da
jugyo <jugyo.org@gmail.com>
2009-02-19 23:31:54 +0900

    use configatron

--- lib/modes/mozrepl.rb
+++ lib/modes/mozrepl.rb
@@ -1,9 +1,18 @@
 require 'net/telnet'
+
+config.mozrepl.set_default(:host, 'localhost')
+config.mozrepl.set_default(:port, 4242)
+config.mozrepl.set_default(:commands, ['content.location.reload(true)'])
+
 Filetter.add_hook do |files, event|
-  telnet = Net::Telnet.new({
-      "Host" => "localhost",
-      "Port" => 4242
-  }){|c| print c}
-  telnet.puts("content.location.reload(true)")
-  telnet.close
+  telnet = Net::Telnet.new( "Host" => config.mozrepl.host,
+                            "Port" => config.mozrepl.port ) {|c| print c }
+  begin
+    config.mozrepl.commands.each do |cmd|
+      puts "> #{cmd}"
+      telnet.puts(cmd)
+    end
+  ensure
+    telnet.close
+  end
 end
